827fd599deec1155c9e43f68107deea4a3683770
Wayne <johnwayne.carreon@gmail.com>
2020-11-02 23:15:24 +0100

    7# Picasso library implemented and it works, need to ajust the layour correctly

--- app/src/main/java/com/wayne/brickapp/model/Theme.java
+++ app/src/main/java/com/wayne/brickapp/model/Theme.java
@@ -8,11 +8,14 @@ public class Theme {
     @SerializedName("parent_id")
     private Integer parentId;
     private String name;
+    @SerializedName("set_img_url")
+    private String imageUrl;
 
     public Theme(Integer id, int parent_id, String name) {
         this.id = id;
         this.parentId = parent_id;
         this.name = name;
+        this.imageUrl = imageUrl;
     }
 
     public int getId() { return id; }
@@ -21,4 +24,6 @@ public class Theme {
     public void setParentId(Integer parentId) { this.parentId = parentId; }
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+    public String getImageUrl() { return imageUrl; }
+    public void setImageUrl(String imageUrl) { this.imageUrl = imageUrl; }
 }
--- app/src/main/java/com/wayne/brickapp/model/ThemeAdapter.java
+++ app/src/main/java/com/wayne/brickapp/model/ThemeAdapter.java
@@ -5,8 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.wayne.brickapp.R;
 
 import java.util.List;
@@ -27,11 +29,13 @@ public class ThemeAdapter extends BaseAdapter {
     }
 
     protected static class ViewHolder {
+        ImageView ivLegoImage;
         TextView tvThemeId;
         TextView tvThemeName;
         public ViewHolder(View view) {
-            this.tvThemeId = view.findViewById(R.id.tvThemeId);
-            this.tvThemeName = view.findViewById(R.id.tvThemeName);
+            ivLegoImage = view.findViewById(R.id.ivLegoImage);
+            tvThemeId = view.findViewById(R.id.tvThemeId);
+            tvThemeName = view.findViewById(R.id.tvThemeName);
         }
     }
 
@@ -51,6 +55,7 @@ public class ThemeAdapter extends BaseAdapter {
         ViewHolder viewHolder = (ViewHolder) view.getTag();
         viewHolder.tvThemeId.setText(String.valueOf(theme.getId()));
         viewHolder.tvThemeName.setText(theme.getName());
+        Picasso.with(parent.getContext()).load(theme.getImageUrl()).into(viewHolder.ivLegoImage);
         return view;
     }
 }
